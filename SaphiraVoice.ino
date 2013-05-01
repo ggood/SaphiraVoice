4719a0130621f1394fa3cfa162652c6fe321f732
Gordon Good <velo27@yahoo.com>
2013-05-01 00:07:07 -0700

    Add fade out when momentary sound's button is released.

--- SaphiraVoice.ino
+++ SaphiraVoice.ino
@@ -7,7 +7,8 @@
 SdFat sd;
 SFEMP3Shield MP3player;
 
-byte button_pins[] = {A0, A1, A2, A3, A4, A5};
+const byte button_pins[] = {A0, A1, A2, A3, A4, A5};
+const byte momentary_sounds[1] = {1};  // Starts at one, not zero
 Bounce bounce[sizeof(button_pins)] = {
   Bounce(button_pins[0], 5),
   Bounce(button_pins[1], 5),
@@ -22,6 +23,7 @@ void setup() {
   uint8_t result; //result code from some function as to be tested at later time.
 
   Serial.begin(115200);
+  
   for (byte i = 0; i < sizeof(button_pins); i++) {
     pinMode(button_pins[i], INPUT_PULLUP); 
   }
@@ -54,19 +56,57 @@ int get_button() {
   return -1;
 }
 
+void fade_out(unsigned int time) {
+  // Fade to silence over the course of <time> milliseconds
+  const unsigned int fade_quantum = 5; // change volume every 5 ms
+  unsigned int fade_cycles = time / fade_quantum;
+  unsigned int vol = 0;
+  unsigned int fade_per_cycle = 0xFE / fade_cycles;
+  for (unsigned int i = 0; vol <= 0xFE; i++) {
+    if (get_button() != -1) {
+      // Button pressed before fade complete. Exit.
+      break;
+    }
+    MP3player.setVolume(vol, vol);
+    vol += fade_per_cycle;
+  }
+  MP3player.stopTrack();
+  Serial.print("Stopped");
+}
+
+
 void loop() {
   char filename[16];
   
   button_pressed = get_button();
-  if (!MP3player.isPlaying()) {
-    current_sound = -1;
-  }
-  if (button_pressed > -1 && button_pressed != current_sound) {
+  
+  if (button_pressed == -1) {
+    if (MP3player.isPlaying()) {
+      // Is this a momentary sound, or should it keep going?
+      Serial.print("is playing, checking ");
+      Serial.print(sizeof(momentary_sounds));
+      Serial.print(" buttons, current sound = ");
+      Serial.println(current_sound);
+      for (byte i = 0; i < sizeof(momentary_sounds); i++) {
+        if (momentary_sounds[i] == current_sound) {
+          // Yes, turn it off
+          Serial.println("Turning off track");
+          fade_out(100);
+          current_sound = -1;
+        }
+      }
+    } else {
+      // Playback has finished
+      current_sound = -1;
+    }
+  } else if (button_pressed != current_sound) {
     // Play a sound
     sprintf(filename, "track%03d.mp3", button_pressed);
     Serial.print("Playing file ");
     Serial.println(filename);
+    MP3player.setVolume(0, 0);
     MP3player.playMP3(filename);
+    Serial.print("Done playing");
     current_sound = button_pressed;
   }
 }
